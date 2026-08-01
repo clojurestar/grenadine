9828a51a3c579a9e0bcbcf7565e1040fc94bfb92
Ingy döt Net <ingy@ingy.net>
2026-08-01 17:07:43 -0400

    Release 0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cc.clojure/grenadine "0.1.0"
+(defproject cc.clojure/grenadine "0.1.1"
   :description "Portable Maven dependency resolution for Clojure dialects"
   :url "https://github.com/clojurestar/grenadine"
 
