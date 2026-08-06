66eb969c07dcb740c3dc35a9d25cf642365860f0
Ingy döt Net <ingy@ingy.net>
2026-08-06 09:46:54 -0400

    Replace Leiningen with tools.build
    Make the GRENADINE-VERSION assignment in the Makefile the single source
    of truth for builds, native binaries, distribution archives, and
    releases.
    Remove VERSION and project.clj, stop loading lein.mk, and add build.clj
    to create the JAR and POM with tools.build.
    Keep deps.edn free of runtime dependencies so non-JVM dialects do not
    attempt to resolve build tooling or Clojure itself.
    Configure tools.build and deps-deploy as build-only aliases and publish
    the generated artifact and standalone POM through the Clojure CLI.
    Update release preparation to bump the Makefile version and validate that
    release commits contain exactly the expected metadata changes.
    Include build.clj and deps.edn in source archives and store the project
    license at the conventional META-INF/LICENSE path in JARs.
    Update CLI, provenance, JAR, release, and source-staging checks for the new
    version and packaging contracts.
    Also ignore generated bindings and normalize the acknowledgements heading
    capitalization in the README.

---
+++ util/build.clj
@@ -0,0 +1,68 @@
+(ns util.build
+  (:require
+   [clojure.java.io :as io]
+   [clojure.tools.build.api :as b]))
+
+(def lib 'cc.clojure/grenadine)
+(def class-dir "target/classes")
+(def basis
+  {:libs
+   {'org.clojure/clojure {:mvn/version "1.12.0"}}})
+
+(def resources
+  [["License" "META-INF/LICENSE"]
+   ["ThirdPartyNotices.md" "ThirdPartyNotices.md"]
+   ["Provenance.md" "Provenance.md"]
+   ["patch/sources.yaml" "patch/sources.yaml"]
+   ["patch/tools.deps.patch" "patch/tools.deps.patch"]
+   ["patch/tools.deps.edn.patch" "patch/tools.deps.edn.patch"]
+   ["patch/tools.gitlibs.patch" "patch/tools.gitlibs.patch"]
+   ["licenses/Apache-2.0.txt" "licenses/Apache-2.0.txt"]
+   ["licenses/Apache-Maven-NOTICE.txt"
+    "licenses/Apache-Maven-NOTICE.txt"]])
+
+(defn- version []
+  (let [value (System/getenv "GRENADINE_VERSION")]
+    (when-not (and value (re-matches #"[0-9]+\.[0-9]+\.[0-9]+" value))
+      (throw
+       (ex-info
+        "GRENADINE_VERSION must have the form X.Y.Z"
+        {:value value})))
+    value))
+
+(defn- copy-resource [[source target]]
+  (let [destination (io/file class-dir target)]
+    (io/make-parents destination)
+    (io/copy (io/file source) destination)))
+
+(defn jar [_]
+  (let [version (version)
+        jar-file (format "target/grenadine-%s.jar" version)]
+    (b/delete {:path class-dir})
+    (b/copy-dir {:src-dirs ["src"]
+                 :target-dir class-dir})
+    (run! copy-resource resources)
+    (b/write-pom
+     {:class-dir class-dir
+      :lib lib
+      :version version
+      :basis basis
+      :src-dirs ["src"]
+      :src-pom :none
+      :scm {:url "https://github.com/clojurestar/grenadine"
+            :tag "HEAD"}
+      :pom-data
+      [[:description
+        "Portable Maven dependency resolution for Clojure dialects"]
+       [:url "https://github.com/clojurestar/grenadine"]
+       [:licenses
+        [:license
+         [:name "Eclipse Public License 1.0"]
+         [:url "https://opensource.org/license/epl-1-0"]]]]})
+    (io/copy
+     (io/file
+      class-dir "META-INF/maven/cc.clojure/grenadine/pom.xml")
+     (io/file "pom.xml"))
+    (b/jar {:class-dir class-dir
+            :jar-file jar-file})
+    {:jar-file jar-file}))
