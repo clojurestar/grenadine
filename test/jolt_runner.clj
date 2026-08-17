c7b4cfff35085b2bdc2cef4834dcf792452a461d
Ingy döt Net <ingy@ingy.net>
2026-08-17 16:50:31 -0400

    Fix require-deps tests on released Jolt

--- test/jolt_runner.clj
+++ test/jolt_runner.clj
@@ -5,10 +5,18 @@
               "src/grenadine/lock.cljc"
               "src/grenadine/repo.cljc"
               "src/grenadine/coordinate.cljc"
+              "src/grenadine/require_deps.cljc"
+              "src/clojurestar/deps.cljc"
               ;; Refresh namespaces Jolt may replace from its preloaded image
-              ;; while compiling coordinate's new repository require.
+              ;; while compiling coordinate's repository require or the
+              ;; clojurestar facade's host implementation.
+              "src/grenadine/source.cljc"
+              "src/grenadine/gitlibs.cljc"
+              "src/grenadine/version.cljc"
+              "src/grenadine/xml.cljc"
               "src/grenadine/repo.cljc"
               "src/grenadine/lock.cljc"
+              "src/grenadine/coordinate.cljc"
               "src/grenadine/expander.cljc"
               "src/grenadine/graph.cljc"
               "src/grenadine/pom.cljc"
@@ -16,7 +24,6 @@
               "src/grenadine/core.cljc"
               "src/grenadine/runtime.cljc"
               "src/grenadine/require_deps.cljc"
-              "src/clojurestar/deps.cljc"
               "test/grenadine/test_support.cljc"
               "test/grenadine/xml_test.cljc"
               "test/grenadine/pom_test.cljc"
@@ -31,6 +38,14 @@
               "test/grenadine/clojurestar_deps_test.cljc"
               "test/grenadine/coordinate_test.cljc"
               "test/grenadine/test_runner.cljc"]]
+  (load-file file))
+
+;; Test namespace declarations can reload Jolt's embedded Grenadine release.
+;; Restore worktree definitions after all test namespaces have been compiled.
+(doseq [file ["src/grenadine/gitlibs.cljc"
+              "src/grenadine/graph.cljc"
+              "src/grenadine/runtime.cljc"
+              "src/grenadine/require_deps.cljc"]]
   (load-file file))
 
 (grenadine.test-runner/-main)
